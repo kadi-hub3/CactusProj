06bc1819b7eae04b859f01f07224ac485779837a
Kadi M <kicktheball1@gmail.com>
2021-07-11 16:41:23 +0200

    navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,27 @@
+import React from "react";
+
 const Nav = () => {
+  const [nav, setNav] = React.useState(false);
+
+  React.useEffect(() => {
+    const changeBg = () => {
+      if (window.scrollY > 80) {
+        setNav(true);
+      } else {
+        setNav(false);
+      }
+    };
+    window.addEventListener("scroll", changeBg);
+  }, []);
+
   return (
-    <nav className="w-sreen grid grid-cols-2 justify-between py-10 px-20 bg-gray-900 text-white text-lg">
+    <nav
+      className={
+        nav
+          ? "fixed top-0 z-10 w-screen grid grid-cols-2 justify-between py-10 px-20 text-white text-lg bg-black"
+          : "fixed top-0 z-10 w-screen grid grid-cols-2 justify-between py-10 px-20 text-white text-lg bg-transparent"
+      }
+    >
       <h2 className="font-bold text-xl tracking-wider">Alessio Muggani</h2>
       <ul className="flex justify-end tracking-wide">
         <li className="mx-4 hover:text-red-400">Home</li>
